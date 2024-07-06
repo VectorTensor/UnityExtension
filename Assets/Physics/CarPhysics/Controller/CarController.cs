56867e3e63abae3b7234a3e7781cd92b6f38f899
VectorTensor <prayashthapa15@gmail.com>
2024-07-06 13:29:39 +0545

    feat(car): added basic rigid body and engine script

--- Assets/Physics/CarPhysics/Controller/CarController.cs
+++ Assets/Physics/CarPhysics/Controller/CarController.cs
@@ -12,18 +12,26 @@ namespace Physics.CarPhysics.Controller
             
         }
 
-        public float CalculateSuspensionForce(float offset, float strength, float velocity, float damping)
+        /// <summary>
+        ///  Calculate suspension force
+        /// F = offset * strength - velocity * damping
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <param name="strength"></param>
+        /// <param name="velocity"></param>
+        /// <param name="damping"></param>
+        /// <returns></returns>
+        public float CalculateSuspensionForce(float offset, float velocity)
         {
-            return (offset * strength) - velocity * damping;
+            return (offset * _carEngineData.strength) - velocity * _carEngineData.damping;
         }
 
         
         
         #region Builder
-
         
 
-        class Builder
+        public class Builder
         {
             
             private CarEngineData _carEngineData;
