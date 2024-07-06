ccdee1e45e7bc66236217cc27393a560b549757d
VectorTensor <prayashthapa15@gmail.com>
2024-07-06 12:31:22 +0545

    feat(inital): initialize basic car controller builder and calcuate Suspensionforce

---
+++ Assets/Physics/CarPhysics/Controller/CarController.cs
@@ -0,0 +1,61 @@
+﻿using Physics.CarPhysics.Models;
+
+namespace Physics.CarPhysics.Controller
+{
+    public class CarController
+    {
+        private CarEngineData _carEngineData;
+        
+        
+        private CarController()
+        {
+            
+        }
+
+        public float CalculateSuspensionForce(float offset, float strength, float velocity, float damping)
+        {
+            return (offset * strength) - velocity * damping;
+        }
+
+        
+        
+        #region Builder
+
+        
+
+        class Builder
+        {
+            
+            private CarEngineData _carEngineData;
+
+            public Builder WithServices()
+            {
+                
+                return this;
+            }
+
+            public Builder WithData(CarEngineData c)
+            {
+
+                _carEngineData = c;
+                return this;
+                
+            }
+            public CarController Build()
+            {
+                var c = new CarController();
+                if (_carEngineData == null)
+                {
+                    throw new System.Exception("CarEngineData is null");
+                }
+                c._carEngineData = _carEngineData;
+                return c;
+
+            }
+            
+        }
+        
+        #endregion
+        
+    }
+}
---
+++ Assets/Physics/CarPhysics/Models/CarEngineData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Physics.CarPhysics.Models
+{
+    [CreateAssetMenu(fileName = "CarEngineData", menuName = "CarPhysics/CarEngineData", order = 0)]
+    public class CarEngineData : ScriptableObject
+    {
+
+        public float strength;
+        public float damping;
+
+    }
+}
