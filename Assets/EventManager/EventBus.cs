fa99061d833d19285cf1b979be6a1da5e56c51c6
VectorTensor <prayashthapa15@gmail.com>
2024-08-27 21:42:37 +0545

    feat(event): created event bindings

---
+++ Assets/EventManager/EventBus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventManager
+{
+    public class EventBus
+    {
+        
+        private static Dictionary<string, Action> events = new Dictionary<string, Action>();
+        
+    }
+}
---
+++ Assets/EventManager/IEventBinding.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EventManager
+{
+    public interface IEvent
+    {
+    }
+
+    internal interface IEventBinding<T>
+    {
+        public Action<T> OnEvent { get; set; }
+        public Action OnEventNoArgs { get; set; }
+    }
+
+    public class EventBinding<T> : IEventBinding<T> where T : IEvent
+    {
+        Action<T> onEvent;
+        Action onEventNoArgs;
+        Action<T> IEventBinding<T>.OnEvent { get=> onEvent; set=> onEvent = value; }
+        Action IEventBinding<T>.OnEventNoArgs { get=> onEventNoArgs; set=> onEventNoArgs = value; }
+
+        public void Add(Action action)
+        {
+            onEventNoArgs += action;
+        }
+        public void Add(Action<T> action)
+        {
+            onEvent += action;
+        }
+
+        public void Remove(Action<T> action)
+        {
+            onEvent += action;
+        }
+
+        public void Remove(Action action)
+        {
+            onEventNoArgs += action;
+        }
+        
+        
+        
+        
+        
+    }
+}
